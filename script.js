ab7a4153d9e54649fd8d90e72e3cfe4536bfe457
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-02 13:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function meta(){
+    let p=document.getElementById("status");
+    p.innerHTML = "<h1>Entered Metaverse</h1>";
+}
+
+let btn = document.getElementById("enterBtn");
+btn.addEventListener("click",meta)
